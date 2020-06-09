2a4e09892873e46ed36e071389c4704f02af9603
sericaer <sericaer@outlook.com>
2020-06-09 22:59:57 +0800

    table colum 本地化

--- Assets/Scripts/MonoBehaviour/sceneMain/FamilyPanel.cs
+++ Assets/Scripts/MonoBehaviour/sceneMain/FamilyPanel.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI.Extensions;
 using System.Collections.Generic;
+using UnityUITable;
 
 public class FamilyPanel : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class FamilyPanel : MonoBehaviour
 
     public List<TaisEngine.Person> persons = new List<TaisEngine.Person>();
 
+    public GameObject tableColum;
 
     // Use this for initialization
     void Start()
@@ -19,6 +21,13 @@ public class FamilyPanel : MonoBehaviour
         background.format = gmFamily.background.name;
         attitude.format = gmFamily.attitude.ToString();
         persons = gmFamily.persons;
+
+        var columns = tableColum.GetComponentsInChildren<HeaderCellContainer>();
+        foreach(var col in columns)
+        {
+            col.columnInfo.columnTitle = TaisEngine.Mod.GetLocalString(col.label.text);
+        }
+
     }
 
     // Update is called once per frame
